29d9fa021b6f6e8258af50f66fa652687dcf0f08
didiermarques <62931608+didiermarques@users.noreply.github.com>
2020-04-09 19:39:57 +0200

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,126 @@
+def fill_tab
+  journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+  return journalists
+end
+
+def get_input
+  puts "Que voulez vous faire/savoir?
+
+  1 - Combien y a-t-il de journalistes dans ce array ?
+  2 - Combien d'handle contiennent un numéro ?
+  3 - Combien d'handle contiennent les 4 lettres du prénom 'Aude' à la suite (sans prendre en compte les majuscules) ?
+  4 - Combien commencent par une majuscule (première lettre juste après le @) ?
+  5 - Combien contiennent au moins une majuscule ?
+  6 - Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+  7 - Trie la liste de handle par ordre alphabétique.
+  8 - Quels sont les 50 handles les plus courts de ce array ?
+  9 - Quelle est la position dans l'array de la personne @epenser ?
+  "
+
+  print ">> "
+  input = gets.to_i
+  if  input >= 1 && input <= 9
+    return input
+  else
+    puts "Veuillez choisir entre 1 et 9"
+    get_input
+  end
+end
+
+def quest_1 (tab)
+  puts "Il y a #{tab.size} journalistes dans cet array."
+
+end
+
+
+def quest_2 (tab)
+  (tab.size).times do |i|
+    tab [i] = tab[i].scan(/\d+/).map(&:to_i)
+  end
+  tab.delete_if(&:empty?)
+  puts "Il y a #{tab.size} handles qui contiennent un numéro"
+end
+
+def quest_3 (tab)
+  tab.map!(&:downcase)
+  (tab.size).times do |i|
+    tab [i] = tab[i].scan(/aude/)
+  end
+  tab.delete_if(&:empty?)
+  puts "Il y a #{tab.size} handles qui contiennent 'Aude'."
+end
+
+def quest_4 (tab)
+  (tab.size).times do |i|
+    tab [i] = tab[i][1].scan(/\p{Upper}/)
+  end
+  tab.delete_if(&:empty?)
+  puts "Il y a #{tab.size} handles qui commencent par une majuscule."
+end
+
+def quest_5 (tab)
+  (tab.size).times do |i|
+    tab [i] = tab[i].scan(/\p{Upper}/)
+  end
+  tab.delete_if(&:empty?)
+  puts "Il y a #{tab.size} handles qui commencent par une majuscule."
+end
+
+def quest_6 (tab)
+  under = 0
+  (tab.size).times do |i|
+    if tab[i].count("_") >=1
+      under += tab[i].count("_")
+    end
+  end
+  puts "Il y a #{under} handles qui contiennent un underscore."
+end
+
+def quest_7 (tab)
+  tab = tab.sort_by(&:upcase)
+  puts "Voulez vous que je vous affiche le résultat? (y/n)"
+  print ">> "
+  if gets.chomp == "y"
+    puts tab
+  else
+    return
+  end
+end
+
+def quest_8 (tab)
+  tab = tab.sort_by {|x| x.length}
+  puts tab [0..49]
+end
+
+def quest_9 (tab)
+  puts tab.index("@epenser")
+end
+
+def choose_quest (input, tab)
+  if input == 1
+    quest_1 (tab)
+  elsif input == 2
+    quest_2(tab)
+  elsif input == 3
+    quest_3(tab)
+  elsif input == 4
+    quest_4(tab)
+  elsif input == 5
+    quest_5(tab)
+  elsif input == 6
+    quest_6(tab)
+  elsif input == 7
+    quest_7(tab)
+  elsif input == 8
+    quest_8(tab)
+  elsif input == 9
+    quest_9(tab)
+  end
+end
+
+def perform
+  tab = fill_tab
+  choose_quest(get_input, tab)
+end
+
+perform
